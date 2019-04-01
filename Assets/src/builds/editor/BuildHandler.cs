5d5458190f9ba0aa9c3fff5643f1686ac1c415e6
ovieira <felixvieira@gmail.com>
2019-04-01 18:28:44 +0100

    Using unity build handler interface for pre and post build methods

---
+++ Assets/src/builds/editor/BuildHandler.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Media;
+using System.Threading;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class BuildHandler : IPreprocessBuildWithReport, IPostprocessBuildWithReport
+{
+
+
+
+    public int callbackOrder => 0;
+
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        //add build number to app version
+        AppVersion appVersion = getCurrentVersion();
+
+
+        //add build number to bundle version for version printing on build
+        PlayerSettings.bundleVersion = appVersion.ConvertToString(AppVersion.VersionMode.FULL);
+    }
+
+
+    public void OnPostprocessBuild(BuildReport report)
+    {
+        AppVersion appVersion = getCurrentVersion();
+        appVersion.IncrementBuild();
+        //
+        printVersion();
+        //
+        PlayerSettings.bundleVersion = appVersion.ConvertToString(AppVersion.VersionMode.RELEASEMAJORMINOR);
+        PlayerSettings.macOS.buildNumber = appVersion.ConvertToString(AppVersion.VersionMode.BUILD);
+        //
+        var clip = Resources.Load<AudioClip>("buildcompleted");
+        var clipPath = AssetDatabase.GetAssetPath(clip);
+        _t = new Thread(() => PlaySound(clipPath))
+        {
+            IsBackground = true
+        };
+        _t.Start();
+
+    }
+
+    static Thread _t;
+
+
+    public static void PlaySound(string clipPath)
+    {
+        var absolutePath = Path.Combine(Environment.CurrentDirectory, clipPath);
+        using (SoundPlayer player = new SoundPlayer(absolutePath))
+        {
+            player.PlaySync();
+        }
+        _t.Join();
+    }
+
+    private static AppVersion getCurrentVersion()
+    {
+        return new AppVersion(PlayerSettings.bundleVersion, PlayerSettings.macOS.buildNumber);
+    }
+
+    #region Print
+    [MenuItem("Tekuchi/Check App Version")]
+    public static void printVersion()
+    {
+        getCurrentVersion().print();
+    }
+    #endregion
+
+
+}
---
+++ Assets/src/version/editor/AppVersion.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+
+public struct AppVersion
+{
+    #region Contructor
+
+
+    /// <summary>
+    /// Creates AppVersion struct from string representing version and build number
+    /// </summary>
+    /// <param name="version">Get it from PlayerSettings.bundleVersion</param>
+    /// <param name="build">Get it from PlayerSettings.macOS.buildNumber</param>
+    public AppVersion(string version, string build)
+    {
+        var split = version.Split('.');
+        Release = int.Parse(split[0]);
+        Major = int.Parse(split[1]);
+        Minor = int.Parse(split[2]);
+        Build = int.Parse(build);
+    }
+    #endregion
+
+    #region Properties
+    public int Release { get; private set; }
+    public int Major { get; private set; }
+    public int Minor { get; private set; }
+    public int Build { get; private set; }
+    #endregion
+
+    #region Convert
+    public enum VersionMode
+    {
+        FULL,
+        RELEASEMAJORMINOR,
+        BUILD
+    }
+    public string ConvertToString(VersionMode format)
+    {
+        string version = $"{Release}.{Major}.{Minor}";
+
+        switch (format)
+        {
+            case VersionMode.FULL:
+                return ($"{version}.{Build}");
+            case VersionMode.RELEASEMAJORMINOR:
+                return version;
+            case VersionMode.BUILD:
+                return Build.ToString();
+            default:
+                throw new ArgumentOutOfRangeException(nameof(format), format, null);
+        }
+
+    }
+    #endregion
+
+    #region Increment
+    public void IncrementBuild()
+    {
+        this.Build = this.Build + 1;
+    }
+    #endregion
+
+    #region Print
+    public void print(VersionMode format = VersionMode.FULL)
+    {
+        Debug.Log(this.ConvertToString(format));
+    }
+    #endregion
+}
